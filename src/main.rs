e41e4e4304302f196c6ee14c65530be06d40b7cc
Yasuaki Gohko <ygohko@gmail.com>
2025-02-08 09:30:36 +0900

    Added test codes to make directories.

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ use std::fs;
 
 use crate::object_store::ObjectStore;
 
-fn main() -> std::io::Result<()>{
+fn main() -> std::io::Result<()> {
     println!("Hello, world!");
 
     // ADHOC
     fs::create_dir_all("NTM/Backups")?;
-    fs::create_dir_all("NTM/Objects" )?;
+    fs::create_dir_all("NTM/Objects")?;
     let store = ObjectStore::new(&"NTM/Objects");
+    let bytes: Vec<u8> = Vec::new();
+    store.add("01234567", &bytes);
 
     Ok(())
 }
--- src/object_store.rs
+++ src/object_store.rs
@@ -1,16 +1,37 @@
+use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
+
+// TODO: Add Error type.
 
 pub struct ObjectStore {
     path: PathBuf,
 }
 
 impl ObjectStore {
-    pub fn new(path :&dyn AsRef<Path>) -> Self {
+    pub fn new(path: &dyn AsRef<Path>) -> Self {
         let mut path_buf = PathBuf::new();
         path_buf.push(path);
-        ObjectStore {
-            path: path_buf,
+        ObjectStore { path: path_buf }
+    }
+
+    pub fn add(&self, id: &str, bytes: &Vec<u8>) {
+        // TODO: Implement this.
+        let path1 = &id[0..2];
+        let path2 = &id[2..4];
+        let path3 = &id[4..6];
+        let path4 = &id[6..8];
+        let mut path = self.path.clone();
+        path.push(path1);
+        path.push(path2);
+        path.push(path3);
+        path.push(path4);
+        println!("path: {}", path.display());
+        match fs::create_dir_all(path) {
+            Ok(_) => (),
+            Err(_) => panic!(),
         }
+
+        // TODO: Write bytes.
     }
 }
