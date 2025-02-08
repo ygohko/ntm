4b340b5163c50e5715947421423ae140d2ac9451
Yasuaki Gohko <ygohko@gmail.com>
2025-02-08 16:30:29 +0900

    Added FilePathProducer.

---
+++ src/file_path_producer.rs
@@ -0,0 +1,83 @@
+use std::env::consts;
+use std::fs;
+use std::path::Path;
+
+use crate::error;
+use crate::error::Error;
+
+pub struct FilePathProducer {
+    file_paths: Vec<String>,
+    directory_paths: Vec<String>,
+    prefix_length: usize,
+}
+
+impl FilePathProducer {
+    // TODO: This argument should be AsRef<Path>?
+    pub fn new(path: String) -> FilePathProducer {
+        let prefix_length = path.len() + 1;
+        return FilePathProducer {
+            file_paths: Vec::new(),
+            directory_paths: vec![path],
+            prefix_length: prefix_length,
+        };
+    }
+
+    pub fn next(&mut self) -> Result<String, Error> {
+        let done = false;
+        while !done {
+            if self.file_paths.len() > 0 {
+                let mut path = self.file_paths.pop().unwrap();
+                if consts::OS == "windows" {
+                    path = path.replace("\\", "/");
+                }
+
+                return Ok(path);
+            }
+
+            if self.directory_paths.len() == 0 {
+                return Err(Error::new(error::CODE_PRODUCING_FINISHED));
+            }
+            let directory_path = self.directory_paths.pop().unwrap();
+
+            let mut scan = true;
+            let option = Path::new(&directory_path).file_name();
+            if option.is_some() {
+                let file_name = option.unwrap().to_string_lossy().to_string();
+                if file_name == ".zatsu".to_string()
+                    || file_name == ".jj".to_string()
+                    || file_name == ".git".to_string()
+                {
+                    scan = false;
+                }
+            }
+
+            if scan {
+                let read_dir = match fs::read_dir(directory_path) {
+                    Ok(read_dir) => read_dir,
+                    Err(_) => return Err(Error::new(0)),
+                };
+                for result in read_dir {
+                    if result.is_ok() {
+                        let entry = result.unwrap();
+
+                        let metadata = match fs::metadata(entry.path()) {
+                            Ok(metadata) => metadata,
+                            Err(_) => {
+                                return Err(Error::new(0))
+                            }
+                        };
+                        let path = entry.path().to_string_lossy().to_string();
+                        if metadata.is_file() {
+                            let path = path[self.prefix_length..].to_string();
+                            self.file_paths.push(path);
+                        } else {
+                            self.directory_paths.push(path);
+                        }
+                    }
+                }
+            }
+        }
+
+        Err(Error::new(error::CODE_PRODUCING_FINISHED))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod error;
+mod file_path_producer;
 mod object_store;
 
 use std::fs;
@@ -5,6 +7,7 @@ use hex_string::HexString;
 use sha2::Digest;
 use sha2::Sha256;
 
+use crate::file_path_producer::FilePathProducer;
 use crate::object_store::ObjectStore;
 
 fn main() -> std::io::Result<()> {
@@ -14,6 +17,48 @@ fn main() -> std::io::Result<()> {
     fs::create_dir_all("NTM/Backups")?;
     fs::create_dir_all("NTM/Objects")?;
     let store = ObjectStore::new(&"NTM/Objects");
+
+    let mut producer = FilePathProducer::new(".".to_string());
+    let mut done = false;
+    while !done {
+        let mut path = "".to_string();
+        path = match producer.next() {
+            Ok(path) => path,
+            Err(error) => {
+                if error.code == error::CODE_PRODUCING_FINISHED {
+                    done = true;
+                }
+                else {
+                    panic!();
+                }
+                "".to_string()
+            },
+        };
+
+        if !done {
+            let bytes = match fs::read(path) {
+                Ok(bytes) => bytes,
+                Err(_) => panic!(),
+            };
+            
+            let mut id_bytes = b"b,".to_vec();
+            id_bytes = [id_bytes, bytes.clone()].concat();
+            println!("id_bytes.len(): {}", id_bytes.len());
+
+            let id = object_id(&id_bytes);
+            store.add(&id, &bytes);
+        }
+    }
+    
+    /*
+    let path = "src/main.rs";
+    let bytes = match fs::read(path) {
+        Ok(bytes) => bytes,
+        Err(_) => panic!(),
+    };
+    */
+
+    /*
     let mut bytes: Vec<u8> = Vec::new();
     bytes.push(0x00);
     bytes.push(0x01);
@@ -23,12 +68,15 @@ fn main() -> std::io::Result<()> {
     bytes.push(0x05);
     bytes.push(0x06);
     bytes.push(0x07);
+     */
+    /*
     let mut id_bytes = b"b,".to_vec();
     id_bytes = [id_bytes, bytes.clone()].concat();
     println!("id_bytes.len(): {}", id_bytes.len());
 
     let id = object_id(&id_bytes);
     store.add(&id, &bytes);
+    */
 
     Ok(())
 }
