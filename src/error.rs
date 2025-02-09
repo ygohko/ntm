e21ae45ad8ff0fb6d0e9fe3d8a4300abcc9f9c47
Yasuaki Gohko <ygohko@gmail.com>
2025-02-09 14:29:54 +0900

    Add backup command (#4)
    * Added BackupCommand.
    
    * Cleaned up.
    
    * Cleaned up.

--- src/error.rs
+++ src/error.rs
@@ -1,14 +1,13 @@
 pub const CODE_GENERAL: i32 = 0;
 pub const CODE_PRODUCING_FINISHED: i32 = 1;
 
+#[derive(Debug)]
 pub struct Error {
     pub code: i32,
 }
 
 impl Error {
     pub fn new(code: i32) -> Error {
-        Error {
-            code,
-        }
+        Error { code }
     }
 }
