97d3a22fd23f3be04f0a930f5faa1024dfa434e9
Yasuaki Gohko <ygohko@gmail.com>
2025-02-10 08:03:27 +0900

    Added tine argument parser.

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,25 @@ mod error;
 mod file_path_producer;
 mod object_store;
 
+use std::env;
+
 use crate::backup_command::BackupCommand;
 use crate::error::Error;
 
 fn main() -> Result<(), Error> {
-    let command = BackupCommand::new();
-    command.execute()?;
+    // TODO: Embed clap.
+    let arguments: Vec<_> = env::args().collect();
+    if arguments.len() < 2 {
+        println!("USAGE: ntm COMMAND");
+
+        return Ok(());
+    }
+    let command_name = arguments[1].clone();
+
+    if command_name == "backup".to_string() {
+        let command = BackupCommand::new();
+        command.execute()?;
+    }
 
     Ok(())
 }
