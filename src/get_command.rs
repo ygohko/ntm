672c93ff24327fb03a8becb4684068b87b5c7953
Yasuaki Gohko <ygohko@gmail.com>
2025-02-11 18:59:47 +0900

    Added codes to write bytes.

--- src/get_command.rs
+++ src/get_command.rs
@@ -5,9 +5,10 @@ use crate::error;
 use crate::error::Error;
 use crate::error::ErrorCode;
 use crate::error::ErrorId;
+use crate::error::Result;
 use crate::file_path_producer;
 use crate::file_path_producer::FilePathProducer;
-use crate::error::Result;
+use crate::object_store::ObjectStore;
 
 pub const ERROR_ID: ErrorId = "get_command";
 
@@ -15,6 +16,7 @@ pub const ERROR_ID: ErrorId = "get_command";
 pub const ERROR_CODE_GENERAL: ErrorCode = 0;
 pub const ERROR_CODE_BACKUP_NOT_FOUND: ErrorCode = 1;
 pub const ERROR_CODE_READING_REFERENCE_FAILED: ErrorCode = 2;
+pub const ERROR_CODE_WRITING_BYTES_FAILED: ErrorCode = 3;
 
 pub struct GetCommand {
     backup: String,
@@ -30,7 +32,7 @@ impl GetCommand {
 
     pub fn execute(&self) -> Result<()> {
         // TODO: Implement this.
-
+        let store = ObjectStore::new(&"NTM/Objects");
         let mut path = PathBuf::new();
         path.push("NTM/Backups/");
         path.push(&self.backup);
@@ -60,7 +62,7 @@ impl GetCommand {
             };
 
             if !done {
-                let bytes = match fs::read(path) {
+                let bytes = match fs::read(path.clone()) {
                     Ok(bytes) => bytes,
                     Err(_) => return Err(Error::new(ERROR_ID, ERROR_CODE_READING_REFERENCE_FAILED)),
                 };
@@ -69,6 +71,20 @@ impl GetCommand {
                     Err(_) => return Err(Error::new(ERROR_ID, ERROR_CODE_READING_REFERENCE_FAILED)),
                 };
                 println!("id: {}", id);
+
+                let bytes = match store.bytes(&id) {
+                    Ok(bytes) => bytes,
+                    // TODO: Skipping file that object is not found may be needed.
+                    Err(error) => return Err(error),
+                };
+                let mut destination_path = PathBuf::new();
+                destination_path.push(&self.backup);
+                destination_path.push(&path);
+                match fs::write(destination_path, bytes) {
+                    Ok(_) => (),
+                    // TODO: Skipping file that writing is failed may be needed.
+                    Err(_) => return  Err(Error::new(ERROR_ID, ERROR_CODE_WRITING_BYTES_FAILED)),
+                };
             }
         }
 
