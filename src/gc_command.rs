04d0a00d2aa6d616e86387b31738d666be1c2fcd
Yasuaki Gohko <ygohko@gmail.com>
2025-02-21 07:52:19 +0900

    Added a function to fint backup paths.

--- src/gc_command.rs
+++ src/gc_command.rs
@@ -45,7 +45,7 @@ impl GcCommand {
 
     pub fn execute(&self) -> Result<()> {
         // TODO: Iterate backup entries.
-
+        /*
         let path = PathBuf::from("Backups");
         let read_dir = match fs::read_dir("Backups") {
             Ok(read_dir) => read_dir,
@@ -65,7 +65,14 @@ impl GcCommand {
                 }
             }
         }
-
+        */
+        let backup_paths = match backup_paths() {
+            Ok(backup_paths) => backup_paths,
+            Err(error) => return Err(error),
+        };
+        for path in backup_paths {
+            println!("path: {}", path);
+        }
 
         // TODO: Mark object files.
         // TODO: Remove not marked objects.
@@ -73,4 +80,27 @@ impl GcCommand {
 
         Err(Error::new(ERROR_ID, ERROR_CODE_GENERAL))
     }
+}
+
+fn backup_paths() -> Result<Vec<String>> {
+    let read_dir = match fs::read_dir("Backups") {
+        Ok(read_dir) => read_dir,
+        Err(_) => return Err(Error::new(ERROR_ID, ERROR_CODE_FINDING_BACKUP_FAILED)),
+    };
+    let mut backup_paths: Vec<String> = Vec::new();
+    for result in read_dir {
+        if result.is_ok() {
+            let entry = result.unwrap();
+            let path = entry.path();
+            let result = entry.metadata();
+            if result.is_ok() {
+                let metadata = result.unwrap();
+                if metadata.is_dir() && !metadata.is_symlink() {
+                    backup_paths.push(String::from_path(&path));
+                }
+            }
+        }
+    }
+
+    Ok(backup_paths)
 }
