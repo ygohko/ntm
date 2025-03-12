246f0a60b6a760e01231d249ab076c661b56dcd9
Yasuaki Gohko <ygohko@gmail.com>
2025-03-13 07:04:18 +0900

    Add new implementaion for GcCommand (#83)
    * Added a stub for new GC implementation.
    
    * Added codes to make backup name list.
    
    * Removed a TODO.
    
    * Added a stub to process objects.
    
    * Adding codes.
    
    * Read attributes.
    
    * Found references.
    
    * Added test codes.
    
    * Fixed issue where GC does not work.
    
    * Fixed issue where removing objects does not work correctly.
    
    * Improved console outputs.
    
    * Moved console outputs.
    
    * Adjusted console outputs.

--- src/gc_command.rs
+++ src/gc_command.rs
@@ -25,12 +25,14 @@ use std::fs;
 use crate::commons::ConvertPath;
 use crate::commons::OperatePath;
 use crate::entry::Entry;
+use crate::error;
 use crate::error::Error;
 use crate::error::ErrorCode;
 use crate::error::ErrorId;
 use crate::error::Result;
 use crate::file_path_producer;
 use crate::file_path_producer::FilePathProducer;
+use crate::object_store::Attributes;
 use crate::object_store::MarkingResult;
 use crate::object_store::ObjectStore;
 
@@ -40,12 +42,14 @@ pub const ERROR_ID: ErrorId = "gc_command";
 pub const ERROR_CODE_GENERAL: ErrorCode = 0;
 pub const ERROR_CODE_FINDING_BACKUP_FAILED: ErrorCode = 1;
 pub const ERROR_CODE_PROCESSING_ENTRY_FAILED: ErrorCode = 2;
+pub const ERROR_CODE_PROCESSING_OBJECT_FAILED: ErrorCode = 3;
 
 pub struct GcCommand {
     destination_path: String,
     store: ObjectStore,
+    backup_paths: Vec<String>,
     processed_count: i64,
-    marked_count: i64,
+    removed_count: i64,
     count: i32,
 }
 
@@ -54,13 +58,59 @@ impl GcCommand {
         Self {
             destination_path: ".".to_string(),
             store: ObjectStore::new(&"Objects"),
+            backup_paths: Vec::new(),
             processed_count: 0,
-            marked_count: 0,
+            removed_count: 0,
             count: 0,
         }
     }
 
     pub fn execute(&mut self) -> Result<()> {
+        let mut backup_path = self.destination_path.clone();
+        backup_path = backup_path.pushed("Backups");
+        let Ok(read_dir) = fs::read_dir(&backup_path) else {
+            return Err(Error::new(ERROR_ID, ERROR_CODE_FINDING_BACKUP_FAILED));
+        };
+        for result in read_dir {
+            if let Ok(entry) = result {
+                if let Ok(metadata) = entry.metadata() {
+                    if metadata.is_dir() {
+                        self.backup_paths.push(String::from_path(&entry.path()));
+                    }
+                }
+            }
+        }
+        
+        let mut object_path = self.destination_path.clone();
+        object_path = object_path.pushed("Objects");
+        let mut producer = FilePathProducer::new(&object_path);
+        let mut done = false;
+        while !done {
+            let option = match producer.next() {
+                Ok(path) => Some(path),
+                Err(error) => {
+                    if error.id == file_path_producer::ERROR_ID && error.code == file_path_producer::ERROR_CODE_PRODUCING_FINISHED {
+                        done = true;
+                    }
+
+                    None
+                },
+            };
+
+            if let Some(produced_path) = option {
+                if produced_path.extension() == "" {
+                    if let Err(error) = self.process_object(&produced_path) {
+                        println!("Warning: error caused when processing objects. error: {}", error);
+                    }
+                    self.processed_count += 1;
+                }
+            }
+        }
+
+        Ok(())
+    }
+    
+    pub fn execute_old(&mut self) -> Result<()> {
         let path = self.destination_path.pushed("Objects");
         self.store = ObjectStore::new(&path);
         let backup_paths = match self.backup_paths() {
@@ -83,6 +133,63 @@ impl GcCommand {
         self.destination_path = path.to_string();
     }
 
+    fn process_object(&mut self, path: &str) -> Result<()> {
+        if self.count == 0 {
+            println!("Processing ({}, {}): {}", self.processed_count, self.removed_count, path);
+        }
+        self.count += 1;
+        self.count %= 100;
+
+        let mut attributes_path = self.destination_path.clone();
+        attributes_path = attributes_path.pushed("Objects");
+        attributes_path = attributes_path.pushed(path);
+        attributes_path += ".attributes";
+
+        // println!("attributes_path: {}", attributes_path);
+        
+        let Ok(serialized) = fs::read_to_string(&attributes_path) else {
+            return Err(Error::new(ERROR_ID, ERROR_CODE_PROCESSING_OBJECT_FAILED));
+        };
+        let attributes: Attributes = match serde_json::from_str(&serialized) {
+            Ok(attributes) => attributes,
+            Err(_) => return Err(Error::new(ERROR_ID, ERROR_CODE_PROCESSING_OBJECT_FAILED)),
+        };
+
+        let object_id = path.file_name();
+        for backup_path in &self.backup_paths {
+            let mut option: Option<String> = None;
+            let entry_path = backup_path.pushed(&attributes.path);
+            if let Ok(serialized) = fs::read_to_string(&entry_path) {
+                option = match serde_json::from_str::<Entry>(&serialized) {
+                    Ok(entry) => Some(entry.id),
+                    Err(_) => None,
+                }
+            }
+
+            if let Some(entry_object_id) = option {
+                if entry_object_id == object_id {
+
+                    // println!("Object {} keeped.", path);
+
+                    return Ok(());
+                }
+            }
+        }
+
+        let mut object_path = self.destination_path.clone();
+        object_path = object_path.pushed("Objects");
+        object_path = object_path.pushed(path);
+        if let Err(_) = fs::remove_file(&object_path) {
+            println!("Warning: Removing {} failed.", object_path);
+        }
+        if let Err(_) = fs::remove_file(&attributes_path) {
+            println!("Warning: Removing {} failed.", attributes_path);
+        }
+        self.removed_count += 1;
+        
+        Ok(())
+    } 
+    
     fn process_backup(&mut self, path: &str) -> Result<()> {
         let mut producer = FilePathProducer::new(&path);
         let mut done = false;
@@ -113,7 +220,7 @@ impl GcCommand {
 
     fn process_entry(&mut self, path: &str) -> Result<()> {
         if self.count == 0 {
-            println!("Processing ({}, {}): {}", self.processed_count, self.marked_count, path);
+            // println!("Processing ({}, {}): {}", self.processed_count, self.marked_count, path);
         }
         self.count += 1;
         self.count %= 100;
@@ -128,7 +235,7 @@ impl GcCommand {
         let result = self.store.mark(&entry.id)?;
         self.processed_count += 1;
         if result == MarkingResult::Marked {
-            self.marked_count += 1;
+            // self.marked_count += 1;
         }
 
         Ok(())
