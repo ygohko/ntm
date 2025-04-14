8411b762fe846828606bb5b6bb5aae155cf70b44
Yasuaki Gohko <ygohko@gmail.com>
2025-04-14 18:42:38 +0900

    Updated a message.

--- src/main.rs
+++ src/main.rs
@@ -174,7 +174,7 @@ fn main() -> ExitCode {
         seconds -= hours * (60 * 60);
         let minutes = seconds / 60;
         seconds -= minutes * 60;
-        println!("Elapsed: {}:{:02}:{:02}", hours, minutes, seconds);
+        println!("Process completed in {}:{:02}:{:02}", hours, minutes, seconds);
     }
 
     ExitCode::SUCCESS
