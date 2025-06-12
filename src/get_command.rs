a4bb3ffed8fb35d92a45ef90c2a5c849993f2f09
Yasuaki Gohko <ygohko@gmail.com>
2025-06-13 07:47:17 +0900

    Migrating to to_string_easy()

--- src/get_command.rs
+++ src/get_command.rs
@@ -257,6 +257,7 @@ mod tests {
     use tempdir::TempDir;
 
     use crate::backup_command::BackupCommand;
+    use crate::commons::OperatePath;
     use crate::get_command::GetCommand;
     use crate::init_command::InitCommand;
     use crate::task::Task;
@@ -281,7 +282,7 @@ mod tests {
         ntm_path.push("ntm");
         fs::create_dir_all(&ntm_path).unwrap();
         let mut command = InitCommand::new();
-        command.set_destination_path(&ntm_path.to_string_lossy().to_string());
+        command.set_destination_path(&ntm_path.to_string_easy());
         command.execute().unwrap();
 
         let mut config_path = ntm_path.clone();
@@ -290,15 +291,15 @@ mod tests {
         fs::write(config_path, config).unwrap();
 
         let mut command = BackupCommand::new();
-        command.set_destination_path(&ntm_path.to_string_lossy().to_string());
+        command.set_destination_path(&ntm_path.to_string_easy());
         command.execute().unwrap();
 
         let mut gotten_path = temp_path.clone();
         gotten_path.push("gotten");
         fs::create_dir_all(&gotten_path).unwrap();
         let mut command = GetCommand::new(&command.name);
-        command.set_destination_path(&ntm_path.to_string_lossy().to_string());
-        command.set_gotten_path(&gotten_path.to_string_lossy().to_string());
+        command.set_destination_path(&ntm_path.to_string_easy());
+        command.set_gotten_path(&gotten_path.to_string_easy());
         command.execute().unwrap();
     }
 
@@ -325,7 +326,7 @@ mod tests {
         ntm_path.push("ntm");
         fs::create_dir_all(&ntm_path).unwrap();
         let mut command = InitCommand::new();
-        command.set_destination_path(&ntm_path.to_string_lossy().to_string());
+        command.set_destination_path(&ntm_path.to_string_easy());
         command.execute().unwrap();
 
         let mut config_path = ntm_path.clone();
@@ -334,7 +335,7 @@ mod tests {
         fs::write(config_path, config).unwrap();
 
         let mut command = BackupCommand::new();
-        command.set_destination_path(&ntm_path.to_string_lossy().to_string());
+        command.set_destination_path(&ntm_path.to_string_easy());
         command.execute().unwrap();
         let backup_name = command.name.clone();
 
@@ -342,8 +343,8 @@ mod tests {
         gotten_path.push("gotten");
         fs::create_dir_all(&gotten_path).unwrap();
         let mut command = GetCommand::new(&command.name);
-        command.set_destination_path(&ntm_path.to_string_lossy().to_string());
-        command.set_gotten_path(&gotten_path.to_string_lossy().to_string());
+        command.set_destination_path(&ntm_path.to_string_easy());
+        command.set_gotten_path(&gotten_path.to_string_easy());
         command.execute().unwrap();
 
         let mut file_path = gotten_path.clone();
