db33d1fac921922047f3e467f837047b63455c63
Yasuaki Gohko <ygohko@gmail.com>
2025-07-18 07:12:44 +0900

    Add offset to backup created.

--- src/gc_command.rs
+++ src/gc_command.rs
@@ -21,7 +21,6 @@
  */
 
 use camino::Utf8PathBuf;
-use chrono::FixedOffset;
 use chrono::NaiveDateTime;
 use serde_derive::Deserialize;
 use serde_derive::Serialize;
@@ -258,10 +257,9 @@ impl GcCommand {
             let path2 = Utf8PathBuf::from(&backup_path);
             let backup_name = path2.file_name_or_empty();
             println!("backup_name: {}", backup_name);
-            let timezone = FixedOffset::east_opt(9 * 60 * 60).unwrap();
             let backup_created = match NaiveDateTime::parse_from_str(&backup_name, "%Y%m%d-%H%M") {
-                // Add 60 seconds because backup_created does not have seconds precision.
-                Ok(created) => created.and_local_timezone(timezone).unwrap().timestamp() + 60,
+                // Add 25 hours because backup_created does not have timezone.
+                Ok(created) => created.and_utc().timestamp() + 25 * 60 * 60,
                 Err(_) => added,
             };
             println!("backup_created: {}, added: {}", backup_created, added);
